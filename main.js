e7b373f312a11823b74b224504c22f97b2b38111
suhanashakirshaikh <83486875+suhanashakirshaikh@users.noreply.github.com>
2021-10-12 12:54:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var canvas = new fabric.canvas('myCanvas');_
+var canvas = new fabric.Canvas('myCanvas');
 
 player_x = 10;
 player_y = 10;
@@ -7,16 +7,16 @@ block_image_width = 30;
 block_image_height = 30;
 
 var player_object = "";
-var block_image_object= "";
+var block_image_object = "";
 
 function player_update()
 {
-    fabric.Image.fromURL("player.png",function(Img) {
+    fabric.Image.fromURL("player.png", function (Img){
         player_object = Img;
-        
+
         player_object.scaleToWidth(150);
         player_object.scaleToHeight(140);
-        player_object.set({ 
+        player_object.set({
             top:player_y,
             left:player_x
         });
@@ -28,15 +28,14 @@ function player_update()
 function new_image(get_image)
 {
     fabric.Image.fromURL(get_image, function(Img) {
-        block_image_object = Img;
-        
+        block_image_object=Img;
+
         block_image_object.scaleToWidth(block_image_width);
         block_image_object.scaleToHeight(block_image_height);
-        block_image_object.set({ 
+        block_image_object.set({
             top:player_y,
             left:player_x
         });
         canvas.add(block_image_object);
-
     });
 }
